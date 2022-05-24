61a0df40b2b3cb19876c7755f7b5aa789bf5df4b
hirotobb7 <hiroto.bb7.pf@gmail.com>
2022-05-25 01:09:44 +0900

    dto導入

--- src/internal/db/repositories/dynamo/wish_list.go
+++ src/internal/db/repositories/dynamo/wish_list.go
@@ -2,12 +2,14 @@ package dynamo
 
 import (
 	"github.com/guregu/dynamo"
+
+	dtos "github.com/hirotobb7/mawist/internal/db/dtos/dynamo"
 	"github.com/hirotobb7/mawist/internal/db/repositories"
 	"github.com/hirotobb7/mawist/internal/models"
 	"github.com/pkg/errors"
 )
 
-const tableName = "wish_lists"
+const tableName string = "wish_lists"
 
 type wishListRepository struct {
 	table dynamo.Table
@@ -18,23 +20,30 @@ func NewWishListRepository(db *dynamo.DB) repositories.WishListRepository {
 }
 
 func (wR *wishListRepository) FindByUserId(userId string) ([]models.WishList, error) {
-	wishLists := make([]models.WishList, 0)
-	if err := wR.table.Get("user_id", userId).All(&wishLists); err != nil {
-		return wishLists, errors.WithStack(err)
+	_wishLists := make([]dtos.WishList, 0)
+
+	if err := wR.table.Get("user_id", userId).All(&_wishLists); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	wishLists := make([]models.WishList, len(_wishLists), len(_wishLists))
+
+	for i, _wishList := range _wishLists {
+		wishLists[i] = *dtos.ConvertWishListModel(&_wishList)
 	}
 
 	return wishLists, nil
 }
 
-func (wR *wishListRepository) Create(wishList models.WishList) error {
-	if err := wR.table.Put(wishList).If("attribute_not_exists(id)").Run(); err != nil {
+func (wR *wishListRepository) Create(wishList *models.WishList) error {
+	if err := wR.table.Put(dtos.ConvertWishListDto(wishList)).If("attribute_not_exists(id)").Run(); err != nil {
 		return errors.WithStack(err)
 	}
 
 	return nil
 }
 
-func (wR *wishListRepository) Delete(wishList models.WishList) error {
+func (wR *wishListRepository) Delete(wishList *models.WishList) error {
 	if err := wR.table.Delete("user_id", wishList.UserId).Range("id", wishList.Id).Run(); err != nil {
 		return errors.WithStack(err)
 	}
